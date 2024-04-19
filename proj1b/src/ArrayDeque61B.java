117bb62fd168194ae6f692df1cbba1ca7c2c26d5
YouJunJie <junjie@ifastfinancial.com.cn>
2024-04-19 15:01:48 +0800

    proj1b init

---
+++ proj1b/src/ArrayDeque61B.java
@@ -0,0 +1,52 @@
+import java.util.List;
+
+public class ArrayDeque61B<T> implements Deque61B<T> {
+
+    public ArrayDeque61B() {
+    }
+
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
